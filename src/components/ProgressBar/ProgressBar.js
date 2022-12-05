f63071568490b57fffd011a386765ef259f49e72
Steve St.Pierre <stephencstpierre@gmail.com>
2022-12-05 13:06:47 -0500

    completed the components

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,56 @@
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+import { COLORS } from "../../constants";
+import VisuallyHidden from "../VisuallyHidden";
+
+const wrapperStyles = {
+    small: {
+        height: "8px",
+        borderRadius: "4px",
+    },
+    medium: {
+        height: "12px",
+        borderRadius: "4px",
+    },
+    large: {
+        height: "24px",
+        borderRadius: "8px",
+    },
+};
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+    return (
+        <div
+            style={{
+                width: "370px",
+                background: COLORS.transparentGray15,
+                boxShadow: "inset 0px 2px 4px rgba(128, 128, 128, 0.35)",
+                overflow: "hidden",
+                padding: size === "large" ? "4px" : "0",
+                ...wrapperStyles[size],
+            }}
+        >
+            <div
+                style={{
+                    height: "100%",
+                    width: "100%",
+                    overflow: "hidden",
+                    borderRadius: "4px",
+                }}
+            >
+                <div
+                    style={{
+                        background: COLORS.primary,
+                        width: `${value}%`,
+                        height: "100%",
+                        borderRadius: "4px 0px 0px 4px",
+                    }}
+                />
+            </div>
+        </div>
+    );
 };
 
 export default ProgressBar;
